276a51e4caaa4590c44c9400b3bc6abcad8d17ef
Danilo <danilo.volles@outlook.com>
2022-11-11 18:23:27 -0300

    Configurações da main class

--- src/main/java/org/senai/prjjava/PrjJavaApplication.java
+++ src/main/java/org/senai/prjjava/PrjJavaApplication.java
@@ -2,12 +2,23 @@ package org.senai.prjjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+// Os @ são os inject - são injeções de classes que realizam funções
 @SpringBootApplication
+@RestController
 public class PrjJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrjJavaApplication.class, args);
 	}
 
+	// O @GetMapping realiza um mapeamento do tipo get - vai ser do endereço/hello
+	// O @RequestParam identifica quais os parâmetros que virão na barra de endereços
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
