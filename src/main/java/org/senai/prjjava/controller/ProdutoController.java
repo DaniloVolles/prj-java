ff23afc01e9fe42c3aa006f4944b66bd0fc946c3
Danilo <danilo.volles@outlook.com>
2022-11-16 20:31:39 -0300

    Complementos da classe ProdutoController.

--- src/main/java/org/senai/prjjava/controller/ProdutoController.java
+++ src/main/java/org/senai/prjjava/controller/ProdutoController.java
@@ -5,10 +5,9 @@ import org.senai.prjjava.repository.ProdutoRepository;
 import org.senai.prjjava.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping (path = "/api/produto")
@@ -18,10 +17,31 @@ public class ProdutoController {
     @Autowired
     ProdutoRepository produtoRepository;
 
+    @PostMapping("/")
     public @ResponseBody Integer addProduto(@RequestBody Produto objetoProduto) {
         produtoRepository.save(objetoProduto);
         return objetoProduto.getId();
     }
 
+    @GetMapping("/")
+    public @ResponseBody Iterable<Produto> buscarProdutos () {
+        return produtoRepository.findAll();
+    }
 
+    @GetMapping("/{id}")
+    public @ResponseBody Optional<Produto> buscarProduto (@PathVariable Integer id) {
+        return produtoRepository.findById(id);
+    }
+
+    @PutMapping("")
+    public @ResponseBody Produto atualizar(@RequestBody Produto objetoProduto) {
+        produtoRepository.save(objetoProduto);
+        return objetoProduto;
+    }
+
+    @DeleteMapping("/{id}")
+    public @ResponseBody String apagar(@PathVariable Integer id) {
+        produtoRepository.deleteById(id);
+        return "Ok ao apagar!";
+    }
 }
